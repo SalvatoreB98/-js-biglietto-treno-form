8385b0ec1e07206cab63cc0ff1f672c2fdda7fab
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-16 17:33:26 +0200

    Aggiorna carrozza

--- myScript.js
+++ myScript.js
@@ -1,9 +1,4 @@
-var prezzoBiglietto;
-var prezzoKilometro = 0.21;
-var scontoMinori = 0.2;
-var sconto;
-var scontoAnziani = 0.4;
-var nomeCognome;
+
 
 onWindowLoad();
 
@@ -14,63 +9,72 @@ function onWindowLoad() {
     myForm.addEventListener("submit", function (event) {
         event.preventDefault();
         //prende e inietta il nome
-        nomeCognome = document.getElementById("name").value;
+        var nomeCognome = document.getElementById("name").value;
         console.log(nomeCognome);
         document.getElementById("nome-passeggero").innerHTML = nomeCognome;
         //prende i km da percorrere
         var kilometri = document.getElementById("kilometri").value;
         // prende il dato inserito , calcola che offerta verrà applicata e infine la inietta
         var fasciaEta = document.getElementById("fascia-eta").value;
-        console.log(fasciaEta);
         var offerta = calcolaOfferta(fasciaEta);
-        console.log(offerta);
-        document.getElementById("offerta").innerHTML =  offerta;
+        document.getElementById("offerta").innerHTML = offerta;
+        // calcola la carrozza e inietta nel corrispettivo ID
+        document.getElementById("carrozza").innerHTML = calcolaCarrozza();
+        // calcola il codice treno e inietta nel corrispettivo ID
+        document.getElementById("codiceCP").innerHTML = calcolaCodiceCP();
+         // prende tutti gli argomenti che servono e li processa nella funzione per il costo total.Infine inietta il risultato nel corrispettivo ID
+         document.getElementById("costo").innerHTML = calcolaPrezzoFinale();
     })
 }
 
 
 
-
-
-function calcolaPrezzo(iKilometri, ilPrezzoKilometro) {
-    var prezzoBiglietto = iKilometri * ilPrezzoKilometro;
-    prezzoBigliettoUmano = prezzoBiglietto.toFixed(2);
-    return prezzoBigliettoUmano;
-}
-
-function calcolaOfferta(etaPasseggeri){
+function calcolaOfferta(etaPasseggeri) {
     var tipoOfferta;
     if (etaPasseggeri == "minorenne") {
-       tipoOfferta = "Sconto Minorenne";
+        tipoOfferta = "Sconto Minorenne";
     }
-    else if (etaPasseggeri == "adulto"){
+    else if (etaPasseggeri == "over65") {
         tipoOfferta = "Sconto Over 65";
     }
-    else if( etaPasseggeri == "over65") {
+    else if (etaPasseggeri == "adulto") {
         tipoOfferta = "Tariffa Adulto";
     }
     return tipoOfferta;
 }
 
-function calcolaPrezzoFinale() {
+////////////////
+function calcolaCarrozza() {
+    return Math.floor(Math.random() * 6 + 1);
+}
+/////////////////
+function calcolaCodiceCP() {
+    return Math.floor(Math.random() * 10000 + 90000);
+}
+/////////////////
+function calcolaPrezzoFinale(etaPasseggeri,iKilometri, ilPrezzoKilometro) {
     var prezzoBigliettoUmano;
     var sconto;
-    if (etaPasseggeri < 18) {
-        sconto = prezzoBiglietto * scontoMinori;
+    var scontoAnziani = 0.4;
+    var scontoMinori = 0.2;
+    var prezzoKilometro = 0.21;
+    var prezzoBiglietto = iKilometri * ilPrezzoKilometro;
+    prezzoBigliettoUmano = prezzoBiglietto.toFixed(2);
+    if (etaPasseggeri =="minorenne" ) {
+        var sconto = prezzoBiglietto * scontoMinori;
         prezzoBiglietto = prezzoBiglietto - sconto;
         prezzoBigliettoUmano = prezzoBiglietto.toFixed(2)
-        console.log(prezzoBigliettoUmano + "€"); 
+        console.log(prezzoBigliettoUmano + "€");
     }
-    else if (etaPasseggeri > 65) {
+    else if (etaPasseggeri == "over65") {
         sconto = prezzoBiglietto * scontoAnziani;
         prezzoBiglietto = prezzoBiglietto - sconto;
         prezzoBigliettoUmano = prezzoBiglietto.toFixed(2)
         console.log(prezzoBigliettoUmano + "€");
-        document.getElementById("ticket-price").innerHTML = prezzoBigliettoUmano + "€";
+
     }
     else {
         console.log(prezzoBigliettoUmano + "€");
-        document.getElementById("ticket-price").innerHTML = prezzoBigliettoUmano + "€";
     }
     return prezzoBigliettoUmano;
 }
